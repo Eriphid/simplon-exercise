d359631ec96cae1af8b598801c0dd144ed3a0008
Stéphane CHEA <19475666+Eriphid@users.noreply.github.com>
2019-07-26 12:58:11 +0200

    add overview table

--- src/app/cars/cars.module.ts
+++ src/app/cars/cars.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarEditComponent } from './components/edit/edit.component';
 import { CarOverviewComponent } from './components/overview/overview.component';
-
+import { MatTableModule } from '@angular/material';
+import { CdkColumnDef } from '@angular/cdk/table';
 
 
 @NgModule({
-  declarations: [CarEditComponent, CarOverviewComponent],
+  declarations: [
+    CarEditComponent,
+    CarOverviewComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatTableModule
+  ],
+  providers: [
+    CdkColumnDef
   ]
 })
 export class CarsModule { }
--- src/app/cars/components/overview/overview.component.ts
+++ src/app/cars/components/overview/overview.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { CarService } from '@core/services/car.service';
+import { Car } from '@core/models/car';
 
 @Component({
   selector: 'app-overview',
@@ -7,10 +9,23 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CarOverviewComponent implements OnInit {
+  cars: Car[];
+  cols = [
+    'name',
+    'brand',
+    'price',
+    'fuelType',
+    'horsePower',
+    'startOfSales',
+    'endOfSales'
+  ];
 
-  constructor() { }
+  constructor(private carService: CarService, private changeDetectorRefs: ChangeDetectorRef) {
+
+  }
 
   ngOnInit() {
+    this.carService.getCars().subscribe(data => { this.cars = data; this.changeDetectorRefs.markForCheck(); });
   }
 
 }
