5dfcc44084512e0512e69d8bdbcb671499959d1f
Stéphane CHEA <19475666+Eriphid@users.noreply.github.com>
2019-07-26 18:52:54 +0200

    add edit component

--- src/app/cars/cars.module.ts
+++ src/app/cars/cars.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarEditComponent } from './components/edit/edit.component';
 import { CarOverviewComponent } from './components/overview/overview.component';
-import { MatTableModule, MatIconModule, MatButtonModule } from '@angular/material';
+import { MatTableModule, MatIconModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule } from '@angular/material';
 import { CdkColumnDef } from '@angular/cdk/table';
 import { RouterModule } from '@angular/router';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,12 @@ import { RouterModule } from '@angular/router';
     MatTableModule,
     MatIconModule,
     MatButtonModule,
-    RouterModule
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [
     CdkColumnDef
--- src/app/cars/components/edit/edit.component.ts
+++ src/app/cars/components/edit/edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { CarService } from '@core/services/car.service';
+import { Car } from '@core/models/car';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit',
@@ -7,10 +10,24 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CarEditComponent implements OnInit {
+  car: Car;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute, private router: Router, private carService: CarService) {
   }
 
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.carService.getCar(params.id).subscribe(car => {
+        this.car = car;
+      });
+    });
+  }
+
+
+  onSave() {
+    this.carService.updateCar(this.car).subscribe((data) => {
+      console.log(data);
+      this.router.navigateByUrl('/app/cars/overview');
+    });
+  }
 }
